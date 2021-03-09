dfe02e55c495adf3e4b504aaf7cfe54988200ba6
GitHub Action <action@github.com>
2021-03-09 04:00:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/rey5137/robotrunnerplugin/MyBundle.kt
+++ src/main/kotlin/com/github/rey5137/robotrunnerplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.rey5137.robotrunnerplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/rey5137/robotrunnerplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/rey5137/robotrunnerplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.rey5137.robotrunnerplugin.services
 
+import com.github.rey5137.robotrunnerplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 

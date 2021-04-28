3a663b6e031a4bb93ecea3308278e8d8743a6f2e
Pham Hong Viet <viet.pham@ascendcorp.com>
2021-04-28 15:16:11 +0700

    Improve input renderer

--- src/main/kotlin/com/github/rey5137/robotrunnerplugin/editors/ui/argument/InputTableCellRenderer.kt
+++ src/main/kotlin/com/github/rey5137/robotrunnerplugin/editors/ui/argument/InputTableCellRenderer.kt
@@ -2,6 +2,8 @@ package com.github.rey5137.robotrunnerplugin.editors.ui.argument
 
 import com.github.rey5137.robotrunnerplugin.editors.xml.INPUT_EMPTY
 import com.github.rey5137.robotrunnerplugin.editors.xml.InputArgument
+import com.intellij.ui.ColoredTableCellRenderer
+import com.intellij.ui.SimpleTextAttributes
 import com.intellij.ui.table.JBTable
 import com.intellij.util.ui.UIUtil
 import java.awt.Component
@@ -9,6 +11,27 @@ import javax.swing.JTable
 import javax.swing.table.TableCellRenderer
 
 class InputTableCellRenderer(private val argumentModel: ArgumentModel) : TableCellRenderer {
+
+    private val stringCellRenderer = object : ColoredTableCellRenderer() {
+        override fun customizeCellRenderer(
+            table: JTable?,
+            value: Any?,
+            selected: Boolean,
+            hasFocus: Boolean,
+            row: Int,
+            column: Int
+        ) {
+            val input = value as InputArgument
+            if(input.name == null) {
+                append(input.value, SimpleTextAttributes.REGULAR_ATTRIBUTES)
+            }
+            else {
+                append(input.name, SimpleTextAttributes.REGULAR_BOLD_ATTRIBUTES)
+                append(": ", SimpleTextAttributes.GRAYED_ATTRIBUTES)
+                append(input.value, SimpleTextAttributes.REGULAR_ATTRIBUTES)
+            }
+        }
+    }
 
     private val inputArgumentModel = InputArgumentModel()
     private val table = JBTable(inputArgumentModel)
@@ -26,7 +49,10 @@ class InputTableCellRenderer(private val argumentModel: ArgumentModel) : TableCe
         column: Int
     ): Component {
         val inputs = argumentModel.getInputArguments(row)
-        return getCellRendererComponent(inputs, isSelected)
+        return if(inputs.size == 1)
+            stringCellRenderer.getTableCellRendererComponent(table, inputs[0], isSelected, hasFocus, row, column)
+        else
+            getCellRendererComponent(inputs, isSelected)
     }
 
     private fun getCellRendererComponent(inputs: List<InputArgument>, isSelected: Boolean): Component {
